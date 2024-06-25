8cd4cc3eab16b7423ce22cc5801840f5d8a85f70
Zeilar <philip_angelin@hotmail.com>
2024-06-25 20:49:43 +0200

    fix: add index.d.ts to tsconfig includes

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "*.svg" {
 	const content: any;
-	const ReactComponent: any;
-	export ReactComponent;
+	export const ReactComponent: any;
 	export default content;
 }
