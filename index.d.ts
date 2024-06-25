acad27e3b074f91de67abd4599b5493b67e3b244
Zeilar <philip_angelin@hotmail.com>
2024-06-25 20:42:52 +0200

    fix: write .d.ts syntax properly

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "*.svg" {
 	const content: any;
-	export const ReactComponent: any;
+	const ReactComponent: any;
+	export ReactComponent;
 	export default content;
 }
